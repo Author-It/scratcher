c7439cdfe3873056fd8c152ee23cb61d275fa1b2
titan1230 <80115528+titan1230@users.noreply.github.com>
2023-08-08 12:11:17 +0000

    h

--- src/routes/offerwall/index.ts
+++ src/routes/offerwall/index.ts
@@ -11,7 +11,7 @@ const router = Router();
 router.get("/", async (req, res) => {
     
     console.log("AYAYAYAYAYAYA")
-    res.status(200).send(true);
+    res.status(200).send("1");
 });
 
 export default router;
